4e0779fe55c8ce816d42a92d33c39e79857145b6
TAS-7 <sesonasipho96@gmail.com>
2020-03-27 12:54:05 +0200

    user_authentication update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'DESKTOP.apps.DesktopConfig',
     'crispy_forms',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
