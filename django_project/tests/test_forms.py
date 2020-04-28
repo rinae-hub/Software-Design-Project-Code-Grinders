8484c8f8ec15dc511d1828dd93c7a54f77c98f92
TAS <44087952+TAS-7@users.noreply.github.com>
2020-04-28 23:11:29 +0200

    Update test_forms.py

---
+++ django_project/tests/test_forms.py
@@ -0,0 +1,20 @@
+from django.test import SimpleTestCase
+from accounts.forms import UserRegisterForm
+
+class TestForms(SimpleTestCase):
+    allow_database_queries = True
+    #Valid form data
+    def test_UserRegisterForm_valid_data(self):
+        form = UserRegisterForm(data = {'username':"qwert04",
+        'email':"coverCode@codegrinders.co.za",
+        'password1':"thereispowerinunity",
+        'password2':"thereispowerinunity"
+
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_UserRegisterForm_no_valid_data(self):
+        form = UserRegisterForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors),4)
