fc1bf16b086f9bfe363a6e0aea8c53c7e377c3e7
Dimps99 <50116022+Dimps99@users.noreply.github.com>
2020-05-25 12:38:10 +0200

    Update test_urls.py

--- CodeGrinders/tests/test_urls.py
+++ CodeGrinders/tests/test_urls.py
@@ -17,9 +17,9 @@ class TestUrls(SimpleTestCase):
 
 
     #testing whether the url to logout-view is working
-    #def test_logout_url_is_resolved(self):
-        #url = reverse('logout')
-        #self.assertEquals(resolve(url).func.view_class,django.contrib.auth.views.LogoutView)
+    def test_logout_url_is_resolved(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func.view_class,django.contrib.auth.views.LogoutView)
 
 
         #testing whether the url to abc-view is working
