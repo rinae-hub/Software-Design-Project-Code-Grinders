0a178994fe2097c17751aff4ce32d06b378f6bff
TAS <44087952+TAS-7@users.noreply.github.com>
2020-06-14 14:18:06 +0200

    Update test_views.py

--- CodeGrinders/tests/test_views.py
+++ CodeGrinders/tests/test_views.py
@@ -8,20 +8,21 @@ from web.models import student
 class TestViews(TestCase):
     # creating a dummy data
     def setUp(self):
-        self.student1=student.objects.create(StudentNumber = '70141',Gender='F',YearStarted='2016',RegistrationStart='2008', RegistrationEnd='2018',Streamline='Biological Science Degree')
-        self.student2=student.objects.create(StudentNumber = '70151',Gender='F',YearStarted='2009',RegistrationStart='2013',RegistrationEnd='2012',Streamline='Mathematics Degree')
-        self.student3=student.objects.create(StudentNumber = '714141',Gender='M',YearStarted='2011',RegistrationStart='2015',RegistrationEnd='2011',Streamline='Physical Science Degree')
-        self.student5=student.objects.create(StudentNumber = '79841',Gender='M',YearStarted='2017',RegistrationStart='2016',RegistrationEnd='2016',Streamline='Mathematics Degree')
-        self.student6=student.objects.create(StudentNumber = '728441',Gender='M',YearStarted='2016',RegistrationStart='2018',RegistrationEnd='2008',Streamline='Biological Science Degree')
-        self.student7=student.objects.create(StudentNumber = '788401',Gender='F',YearStarted='2011',RegistrationStart='2018',RegistrationEnd='2015',Streamline='Mathematics Degree')
-        self.student8=student.objects.create(StudentNumber = '788141',Gender='F',YearStarted='2013',RegistrationStart='2018',RegistrationEnd='2018',Streamline='Biological Science Degree')
-        self.student9=student.objects.create(StudentNumber = '6521',Gender='F',YearStarted='2016',RegistrationStart='2008', RegistrationEnd='2018',Streamline='Mathematics Degree')
-        self.student10=student.objects.create(StudentNumber = '54112',Gender='F',YearStarted='2009',RegistrationStart='2013',RegistrationEnd='2012',Streamline='Physical Science Degree')
-        self.student11=student.objects.create(StudentNumber = '4514',Gender='F',YearStarted='2011',RegistrationStart='2015',RegistrationEnd='2011',Streamline='Biological Science Degree')
-        self.student12=student.objects.create(StudentNumber = '4471',Gender='M',YearStarted='2017',RegistrationStart='2016',RegistrationEnd='2016',Streamline='Mathematics Degree')
-        self.student13=student.objects.create(StudentNumber = '1127',Gender='M',YearStarted='2016',RegistrationStart='2018',RegistrationEnd='2008',Streamline='Physical Science Degree')
-        self.student14=student.objects.create(StudentNumber = '00141',Gender='M',YearStarted='2011',RegistrationStart='2018',RegistrationEnd='2015',Streamline='Biological Science Degree')
-        self.student15=student.objects.create(StudentNumber = '895450',Gender='F',YearStarted='2013',RegistrationStart='2018',RegistrationEnd='2018',Streamline='Biological Science Degree')
+        self.student1=student.objects.create(StudentNumber = '70141',Gender='F',YearStarted='2016',RegistrationStart='2008',Qualified='Q',RegistrationEnd='2018',Streamline='Biological Science Degree')
+        self.student2=student.objects.create(StudentNumber = '70151',Gender='F',YearStarted='2009',RegistrationStart='2013',Qualified='Q',RegistrationEnd='2012',Streamline='Mathematics Degree')
+        self.student3=student.objects.create(StudentNumber = '714141',Gender='M',YearStarted='2011',RegistrationStart='2015',Qualified='F',RegistrationEnd='2011',Streamline='Physical Science Degree')
+        self.student5=student.objects.create(StudentNumber = '79841',Gender='M',YearStarted='2017',RegistrationStart='2016',Qualified='F',RegistrationEnd='2016',Streamline='Mathematics Degree')
+        self.student6=student.objects.create(StudentNumber = '728441',Gender='M',YearStarted='2016',RegistrationStart='2018',Qualified='F',RegistrationEnd='2008',Streamline='Biological Science Degree')
+        self.student7=student.objects.create(StudentNumber = '788401',Gender='F',YearStarted='2011',RegistrationStart='2018',Qualified='Q',RegistrationEnd='2015',Streamline='Mathematics Degree')
+        self.student8=student.objects.create(StudentNumber = '788141',Gender='F',YearStarted='2013',RegistrationStart='2018',Qualified='Q',RegistrationEnd='2018',Streamline='Biological Science Degree')
+        self.student9=student.objects.create(StudentNumber = '6521',Gender='F',YearStarted='2016',RegistrationStart='2008', Qualified='Q',RegistrationEnd='2018',Streamline='Mathematics Degree')
+        self.student10=student.objects.create(StudentNumber = '54112',Gender='F',YearStarted='2009',RegistrationStart='2013',Qualified='F',RegistrationEnd='2012',Streamline='Physical Science Degree')
+        self.student11=student.objects.create(StudentNumber = '4514',Gender='F',YearStarted='2011',RegistrationStart='2015',Qualified='Q',RegistrationEnd='2011',Streamline='Biological Science Degree')
+        self.student12=student.objects.create(StudentNumber = '4471',Gender='M',YearStarted='2017',RegistrationStart='2016',Qualified='F',RegistrationEnd='2016',Streamline='Mathematics Degree')
+        self.student13=student.objects.create(StudentNumber = '1127',Gender='M',YearStarted='2016',RegistrationStart='2018',Qualified='Q',RegistrationEnd='2008',Streamline='Physical Science Degree')
+        self.student14=student.objects.create(StudentNumber = '00141',Gender='M',YearStarted='2011',RegistrationStart='2018',Qualified='F',RegistrationEnd='2015',Streamline='Biological Science Degree')
+        self.student15=student.objects.create(StudentNumber = '895450',Gender='F',YearStarted='2013',RegistrationStart='2018',Qualified='Q',RegistrationEnd='2018',Streamline='Biological Science Degree')
+
 
 
     # checking whether the ajax_generate_graph is returning data correctly as it should
@@ -347,6 +348,38 @@ class TestViews(TestCase):
         self.assertEquals(y2016,0)
         self.assertEquals(y2017,0)
         self.assertEquals(y2018,0)
+
+    #Filtered by RegistrationStart,Q,M#
+    def test_qualified_males_in_ajax(self):
+        y2008 = student.objects.filter(RegistrationStart='2008').filter(Qualified='Q').filter(Gender='M').count()
+        y2009 = student.objects.filter(RegistrationStart='2009').filter(Qualified='Q').filter(Gender='M').count()
+        y2010 = student.objects.filter(RegistrationStart='2010').filter(Qualified='Q').filter(Gender='M').count()
+        y2011 = student.objects.filter(RegistrationStart='2011').filter(Qualified='Q').filter(Gender='M').count()
+        y2012 = student.objects.filter(RegistrationStart='2012').filter(Qualified='Q').filter(Gender='M').count()
+        y2013 = student.objects.filter(RegistrationStart='2013').filter(Qualified='Q').filter(Gender='M').count()
+        y2014 = student.objects.filter(RegistrationStart='2014').filter(Qualified='Q').filter(Gender='M').count()
+        y2015 = student.objects.filter(RegistrationStart='2015').filter(Qualified='Q').filter(Gender='M').count()
+        y2016 = student.objects.filter(RegistrationStart='2016').filter(Qualified='Q').filter(Gender='M').count()
+        y2017 = student.objects.filter(RegistrationStart='2017').filter(Qualified='Q').filter(Gender='M').count()
+        y2018 = student.objects.filter(RegistrationStart='2018').filter(Qualified='Q').filter(Gender='M').count()
+
+
+        self.assertEquals(y2008,0)
+        self.assertEquals(y2009,0)
+        self.assertEquals(y2010,0)
+        self.assertEquals(y2011,0)
+        self.assertEquals(y2012,0)
+        self.assertEquals(y2013,0)
+        self.assertEquals(y2014,0)
+        self.assertEquals(y2015,0)
+        self.assertEquals(y2016,0)
+        self.assertEquals(y2017,0)
+        self.assertEquals(y2018,1)
+
+
+
+
+
 
 
 
